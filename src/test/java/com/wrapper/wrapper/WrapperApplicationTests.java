e601ac007c822385f74fc5b5c2289d4b2ab77d38
sakib880git <sakibsheikh2950@gmail.com>
2026-07-28 15:47:55 +0530

    Initial Commit

---
+++ src/test/java/com/wrapper/wrapper/WrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrapper.wrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
